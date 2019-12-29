0cb09aa4a09f07a777f7424a271a891afc1a1ad7
Jasmin Fajkic <jasmin.fajkic@gmail.com>
2019-12-29 11:25:48 +0100

    First node app deploy

--- helpers/getData.js
+++ helpers/getData.js
@@ -3,6 +3,7 @@ const axios = require("axios");
 function getData(url) {
     return axios.get(url)
       .then(res => {
+        console.log(res.data);
         return res.data;
       }).catch(err => console.log(err));
 }
